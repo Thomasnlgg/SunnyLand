5c9371967b2b716f973830873c35384df68d4d55
Thomas <tomines147@gmail.com>
2025-06-11 12:12:57 -0300

    Pronto

---
+++ SunnyLand/Assets/Scripts/Personagem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    private string Nome = "Personagem";
+    private int vida = 5;
+    public float Velocidade = 2.5f;
+    private bool iaAlive = true;
+    private Rigidbody2D rb;
+    public float novagravidade = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Nome: \n" + Nome + "\n Vida: " + vida);
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = novagravidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("player")){
+            print("yes");
+        }
+    }
+}
